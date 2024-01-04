37fa57a78be22efafd1fe932dc81370f32729d6f
Hooder <hoodernew@gmail.com>
2024-01-04 15:13:00 +1300

    Ver 0.1
    Started:
    
    New program in ruby.rb in which the user will have to set up a console, these consoles will most likey be in arrays
    
    Added:
    
    simple string types that hold the consoles and their names (will most likey be put into an array next ver)

---
+++ PlugInConsole_USERProgram.rb
@@ -0,0 +1,18 @@
+#Task Notes
+
+# 1. 
+# User can select from a vary of Consoles (xbox playstation SEGA etc) 
+
+# 2.
+# User then must be able to hook the power cable up to the tv (or monitor)
+
+# 3.
+# The User must select from vary diffrent power cables (like a needle in a haystack)
+
+
+Console01 = "Nintendo DS"
+Console02 = "Xbox 360"
+Console03 = "Playstation 4"
+Console04 = "Playstation 5"
+# string data types holding diffrent consoles and their names
+
