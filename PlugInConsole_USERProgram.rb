0586f75cbc03007e87379773b93af737f04b6e0e
Hooder <hoodernew@gmail.com>
2024-01-05 17:42:24 +1300

    Ver 0.3

--- PlugInConsole_USERProgram.rb
+++ PlugInConsole_USERProgram.rb
@@ -16,4 +16,17 @@
 stringArray_Consoles = ["Nintendo DS", "Xbox 360", "Playstation 4", "Playstation 5"]
 # Creates Array of strings holding console names
 
+stringArray_CablesSTACK = ["DS Powercord" , "360 Powercord" , "P4 and P5 Powercord"]
+# Creates another array holding diffrent powercords (playstation 4 and 5 under the same cord)
 
+HDMICable = "HDMI InputCable"
+AVICable = "AVI InputCable"
+#two more string data types that are two diffrent options for video input
+
+
+
+print "What console would you like to play?\n\n"
+puts stringArray_Consoles[0] 
+puts stringArray_Consoles[1] 
+puts stringArray_Consoles[2]
+#asks user which console they would like to play on
