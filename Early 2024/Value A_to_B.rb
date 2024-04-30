0c7e446acb00fc7193ea38d92aaaaf9f60bff7ed
Hooder <hoodernew@gmail.com>
2024-04-30 22:33:19 +1200

    NEW V1.0
    Trying to get back in the game, new version of HelloWorld added via print

---
+++ Early 2024/Value A_to_B.rb
@@ -0,0 +1,14 @@
+A = 10 
+# assigned a value to variable A
+
+
+B = nil # nil "not pointing to any object"
+# Declares variable B without assigning a value yet
+
+B = A # assigned variable A to variable B 
+
+puts "Value of A: #{A}" 
+#Outputs the value of variable A
+
+puts "Value of B: #{B}"
+#Outputs the value of variable B with now the value A
